c7aaffc4ed49b8b6c5833eed8ac34bea6ddfbe3b
aabedraba <abdallah@zuplo.com>
2023-10-12 12:05:34 +0000

    remove unnecessary policy

--- modules/hello-world.ts
+++
@@ -1,8 +0,0 @@
-import { ZuploContext, ZuploRequest } from "@zuplo/runtime";
-
-export default async function (request: ZuploRequest, context: ZuploContext) {
-  if (request.user?.data?.customerType === "premium") {
-    return request;
-  }
-  return "When are you gonna start paying?"
-}
